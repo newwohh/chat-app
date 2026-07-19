551d7ac07a1b94976c33c4ed5fb55b6eb019e974
newwoohh <hellonevo13@gmail.com>
2026-07-19 20:31:33 +0530

    feat: update layout components and add Astryx setup instructions

--- app/sign-in/layout.tsx
+++ app/sign-in/layout.tsx
@@ -6,5 +6,5 @@ export const metadata: Metadata = {
 };
 
 export default function LoginLayout({children}:{children:React.ReactNode}){
-    return ( <div>{children}</div>)
+    return ( <section>{children}</section>)
 }
